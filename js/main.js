9cc819a03f7fc0433de61baca35f0995023b378b
dianamariasilva <diana.silva.v94@gmail.com>
2017-09-20 12:32:04 -0500

    almacena objetos

--- js/main.js
+++ js/main.js
@@ -1,21 +1,43 @@
+var btnAdd = document.getElementById("add"); 
+var btnAll = document.getElementById("all"); 
+var laboratoria = [];
 function addStudent(){
-    var name = prompt("Please enter student name", "Diana Silva");
-    var techPoints = prompt("Tech Point", "80%");
-    var lifePoints = prompt("Tech Point", "90%");
-    var active;
-    var status = active;
-    console.log(name);
-    console.log(techPoints);
-    console.log(lifePoints);
-    console.log(status);
-    if (name != null && techPoints != null && lifePoints != null) {
+    var student = {
+        name : prompt("Please enter student name", "Diana Silva"),
+        techPoints : prompt("Tech Point", "80%"),
+        lifePoints : prompt("Tech Point", "90%"),
+        status : true
+    }
+
+    laboratoria.push(student);
+
+    console.log(laboratoria);
+    // function allStudents(){
+    //     laboratoria.push(student);
+    //     console.log(laboratoria);
+    // }
+
+    // btnAll.onclick= function(){
+    //     allStudents();
+    // }
+
+    // console.log(name);
+    // console.log(techPoints);
+    // console.log(lifePoints);
+    // console.log(status);
+    if (student.name != null && student.techPoints != null && student.lifePoints != null) {
         document.getElementById("name").innerHTML =
-        "Sudent Name:" + name;
+        "Student Name:" + student.name;
         document.getElementById("tech").innerHTML =
-        "TechPoints:" + techPoints;
+        "TechPoints:" + student.techPoints;
         document.getElementById("life").innerHTML =
-        "Life Points:" + lifePoints;
+        "Life Points:" + student.lifePoints;
         document.getElementById("status").innerHTML =
         "Status: active";
     }
 }
+
+btnAdd.onclick=function(){
+    addStudent();
+}
+
