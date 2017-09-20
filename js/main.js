ae8a8ed202a6f71b3cb155d1e55ef97ec31de1c3
dianamariasilva <diana.silva.v94@gmail.com>
2017-09-20 17:01:41 -0500

    añadir objetos al array

--- js/main.js
+++ js/main.js
@@ -1,30 +1,24 @@
 var btnAdd = document.getElementById("add"); 
 var btnAll = document.getElementById("all"); 
 var laboratoria = [];
+
+var student = {
+    name : undefined,
+    techPoints : undefined,
+    lifePoints : undefined,
+    status : true
+}
 function addStudent(){
-    var student = {
+    student = {
         name : prompt("Please enter student name", "Diana Silva"),
         techPoints : prompt("Tech Point", "80%"),
         lifePoints : prompt("Tech Point", "90%"),
         status : true
     }
-
     laboratoria.push(student);
 
     console.log(laboratoria);
-    // function allStudents(){
-    //     laboratoria.push(student);
-    //     console.log(laboratoria);
-    // }
 
-    // btnAll.onclick= function(){
-    //     allStudents();
-    // }
-
-    // console.log(name);
-    // console.log(techPoints);
-    // console.log(lifePoints);
-    // console.log(status);
     if (student.name != null && student.techPoints != null && student.lifePoints != null) {
         document.getElementById("name").innerHTML =
         "Student Name:" + student.name;
@@ -37,7 +31,25 @@ function addStudent(){
     }
 }
 
+
 btnAdd.onclick=function(){
     addStudent();
 }
 
+btnAll.onclick=function(){
+    myFunction();
+        function getFullName(item, index) {
+            return `<div class="div1">\
+                <p class:"first">Student Name: ${item.name}</p>\
+                <p "first">TechPoints: ${item.techPoints}</p>\
+                <p "first">Life Points: ${item.lifePoints}</p>\
+                <p "first">Status: active</p>\
+            </div>`;
+        }
+
+        function myFunction() {
+            document.getElementById("info").innerHTML = laboratoria.map(getFullName);
+        }
+
+        // $('#info').append ( `<p> ${student[student.length -1].name} </p>`);   
+}
